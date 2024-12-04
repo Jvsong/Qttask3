fd55f2acc38a2bd321953c944aa502a0e080ce3d
宋佳伟 <jvsong52@gmail.com>
2024-12-04 17:13:32 +0800

    3th commit

--- welcomeview.cpp
+++ welcomeview.cpp
@@ -1,14 +1,23 @@
 #include "welcomeview.h"
 #include "ui_welcomeview.h"
+#include <QDebug>
 
 WelcomeView::WelcomeView(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::WelcomeView)
 {
+    qDebug()<<"create welcomeView";
     ui->setupUi(this);
 }
 
 WelcomeView::~WelcomeView()
 {
+    qDebug()<< "destory welcomeView";
     delete ui;
 }
+
+void WelcomeView::on_pushButton_clicked()
+{
+
+}
+
